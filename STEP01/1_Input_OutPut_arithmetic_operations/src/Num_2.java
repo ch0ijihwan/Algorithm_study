bacff861b0d438c2f1f97ffea906d8529a4cbd48
cjh8746 <cjh87467@gmail.com>
2021-06-29 16:16:16 +0900

    IF num 1

---
+++ STEP01/1_Input_OutPut_arithmetic_operations/src/Num_2.java
@@ -0,0 +1,5 @@
+public class Num_2 {
+    public static void main(String[] args) {
+        System.out.printf("강한친구 대한육군\n강한친구 대한육군");
+    }
+}
---
+++ STEP01/1_Input_OutPut_arithmetic_operations/src/Num_3.java
@@ -0,0 +1,9 @@
+public class Num_3 {
+    public static void main(String[] args) {
+        System.out.printf(
+                "\\    /\\\n" +
+                 " )  ( ')\n" +
+                  "(  /  )\n" +
+                 " \\(__)|");
+    }
+}
